23de6416b8800ead77ecbe9ca5b6c1bee0a4add1
Andrej Souček <andrej.soucek@antee.cz>
2018-10-02 17:33:43 +0200

    SignPresenter handles both login and registration

--- app/models/SignModel.php
+++ app/models/SignModel.php
@@ -5,11 +5,17 @@ namespace PP;
 use PP\User\FacebookCredentials;
 use PP\User\FacebookLinkBuilder;
 use PP\User\FacebookUserRead;
+use PP\User\UserRegister;
 
 /**
  * @author Andrej Souček
  */
-class HomepageModel {
+class SignModel {
+
+	/**
+	 * @var UserRegister
+	 */
+	private $register;
 
 	/**
 	 * @var FacebookUserRead
@@ -21,9 +27,22 @@ class HomepageModel {
 	 */
 	private $builder;
 
-	public function __construct(FacebookUserRead $read, FacebookLinkBuilder $builder) {
+	public function __construct(UserRegister $register, FacebookUserRead $read, FacebookLinkBuilder $builder) {
+		$this->register = $register;
 		$this->read = $read;
 		$this->builder = $builder;
+	}
+
+	/**
+	 * @param string $username
+	 * @param string $email
+	 * @param string|null $fb_uid
+	 * @param string|null $password
+	 * @throws IncorrectCredentialsException
+	 * @throws \Nette\Utils\AssertionException
+	 */
+	public function registerUser(string $username, string $email, string $fb_uid = null, string $password = null) : void {
+		$this->register->process($username, $email, $fb_uid, $password);
 	}
 
 	/**
---
+++ app/presenters/SignPresenter.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace PP\Presenters;
+
+use Nette\Application\UI\Presenter;
+use Nette\Application\UI\Form;
+use Nette\Security\AuthenticationException;
+use PP\IncorrectCredentialsException;
+use PP\SignModel;
+use PP\User\PasswordCredentials;
+
+/**
+ * @author Andrej Souček
+ */
+class SignPresenter extends Presenter {
+
+	/**
+	 * @var SignModel
+	 */
+	private $model;
+
+	public function __construct(SignModel $model) {
+		parent::__construct();
+		$this->model = $model;
+	}
+
+	/**
+	 * @throws \Nette\Application\AbortException
+	 */
+	public function actionFbLogin() {
+		try {
+			$this->getUser()->login($this->model->getFacebookCredentials());
+		} catch (AuthenticationException $e) {
+			$this->flashMessage("Error while connecting to Facebook");
+		}
+		$this->redirect('Sign:');
+	}
+
+	/**
+	 * @throws \Nette\Application\AbortException
+	 */
+	public function actionLogOut() {
+		$this->getUser()->logout();
+		$this->redirect('Sign:');
+	}
+
+	public function renderDefault() {
+		$this->template->currentUserName = $this->getCurrentUserName();
+		$this->template->fbLoginUrl = $this->getFbLoginUrl();
+	}
+
+	public function getCurrentUserName() : string {
+		return $this->getUser()->getIdentity() ? $this->getUser()->getIdentity()->username : "unknown";
+	}
+
+	/**
+	 * @return string
+	 * @throws \Nette\Application\UI\InvalidLinkException
+	 * @throws \Nette\Utils\AssertionException
+	 */
+	public function getFbLoginUrl() : string {
+		return $this->model->generateLoginUrl($this->link('//fbLogin'));
+	}
+
+	protected function createComponentLoginForm() : Form {
+		$form = new Form;
+		$form->addText('email', 'E-mail')
+			->setRequired('Please enter your e-mail.')
+			->addRule($form::EMAIL, 'The e-mail must be in correct format.')
+			->setAttribute('placeholder', 'E-mail');
+		$form->addPassword('password', 'Password')
+			->setRequired('Please enter your password.')
+			->setAttribute('placeholder', 'Password');
+		$form->addSubmit('send', 'Log in');
+		$form->onSuccess[] = [$this, 'processLoginForm'];
+		return $form;
+	}
+
+	/**
+	 * @internal
+	 * @param Form $form
+	 * @throws \Nette\Application\AbortException
+	 * @throws \Nette\Security\AuthenticationException
+	 */
+	public function processLoginForm(Form $form) : void {
+		$values = $form->getValues();
+		try {
+			$this->getUser()->login(new PasswordCredentials($values->email, $values->password));
+//			$this->redirect('Map:');
+			$this->flashMessage("SUCCESS");
+		} catch (AuthenticationException $e) {
+			$form->addError("Incorrect e-mail or password.");
+		}
+	}
+
+	protected function createComponentRegisterForm(): Form {
+		$form = new Form();
+		$form->addText('username', 'Username:')
+			->setRequired('Please fill in your username.')
+			->setAttribute('placeholder', 'Username');
+		$form->addText('email', 'E-mail:')
+			->setRequired('Please fill in an e-mail.')
+			->addRule($form::EMAIL, 'The e-mail must be in correct format.')
+			->setAttribute('placeholder', 'E-mail');
+		$form->addPassword('password', 'Password')
+			->setRequired('Please fill in both of the password fields.')
+			->setAttribute('placeholder', 'Password');
+		$form->addPassword('password_confirm', 'Password again:')
+			->addRule(Form::EQUAL, 'Passwords do not match', $form['password'])
+			->setRequired('Please fill in both of the password fields.')
+			->setAttribute('placeholder', 'Password again')
+			->setOmitted(true);
+		$form->addSubmit('send', 'Sign up');
+		$form->onSuccess[] = [$this, 'processRegisterForm'];
+		return $form;
+	}
+
+	/**
+	 * @internal
+	 * @param Form $form
+	 * @throws \Nette\Application\AbortException
+	 * @throws \Nette\Utils\AssertionException
+	 * @throws IncorrectCredentialsException
+	 */
+	public function processRegisterForm(Form $form): void {
+		$values = $form->getValues();
+		try {
+			$this->model->registerUser($values->username, $values->email, null, $values->password);
+			$this->flashMessage('Sign up successful, now you can log in.');
+			$this->redirect('Homepage:');
+		} catch (IncorrectCredentialsException $e) {
+			$form->addError('This account already exists.');
+		}
+	}
+}
