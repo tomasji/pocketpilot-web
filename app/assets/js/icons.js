beeb6c595178c77e077bddc56184da6832306b59
Andrej Souček <andrej.soucek@antee.cz>
2019-07-20 11:23:15 +0200

    Import missing faBars icon

--- app/assets/js/icons.js
+++ app/assets/js/icons.js
@@ -2,11 +2,11 @@ import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import {
 	faTable, faMapMarkedAlt, faBookOpen, faSyncAlt, faEnvelopeOpen, faSignOutAlt, faCalendar, faArrowsAltH,
-	faEye, faTrash, faPlus, faSave
+	faEye, faTrash, faPlus, faSave, faBars
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
 	faFacebookF, faMapMarkedAlt, faBookOpen, faSyncAlt, faEnvelopeOpen, faSignOutAlt, faTable, faCalendar, faArrowsAltH,
-	faEye, faTrash, faPlus, faSave
+	faEye, faTrash, faPlus, faSave, faBars
 )
 document.addEventListener('DOMContentLoaded', dom.i2svg)
