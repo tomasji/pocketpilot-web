57f919998b4a4564ecbe1c841fe6f9f227c29b43
Andrej Souček <andrej.soucek@betsys.com>
2020-10-26 12:11:59 +0100

    add donate button

--- app/assets/js/Common/icons.js
+++ app/assets/js/Common/icons.js
@@ -1,8 +1,8 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import {
   faArrowsAltH, faBars, faBookOpen, faBriefcase, faCalendar, faDatabase, faEnvelopeOpen, faExclamationTriangle,
-  faGlobeEurope, faIndustry, faMapMarkedAlt, faPencilAlt, faPlus, faSave, faShareSquare, faSignOutAlt, faSyncAlt,
-  faTable, faTrash
+  faGlobeEurope, faHandHoldingUsd, faIndustry, faMapMarkedAlt, faPencilAlt, faPlus, faSave, faShareSquare, faSignOutAlt,
+  faSyncAlt, faTable, faTrash
 } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 
@@ -10,7 +10,7 @@ export default function(el) {
   library.add(
     faFacebookF, faMapMarkedAlt, faBookOpen, faSyncAlt, faEnvelopeOpen, faSignOutAlt, faTable, faCalendar, faArrowsAltH,
     faTrash, faPlus, faSave, faBars, faPencilAlt, faBriefcase, faGlobeEurope, faShareSquare, faDatabase, faIndustry,
-    faExclamationTriangle
+    faExclamationTriangle, faHandHoldingUsd
   )
   dom.i2svg({ node: el })
 }
