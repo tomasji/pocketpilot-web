dc143c046a03f65e8bfdee1eafa0aa00d120cc3a
Tomas Jirka <tomasj@salsitasoft.com>
2021-04-01 21:32:18 +0200

    Letsencrypt scripts

--- .docker/app/letsencrypt/init.sh
+++ .docker/app/letsencrypt/init.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-if [ -z "$(ls /home/letsencrypt/.acme.sh)" ]; then
-  su letsencrypt -c "mkdir /home/letsencrypt/cert"
-  su letsencrypt -c "curl https://get.acme.sh -o /home/letsencrypt/acme.sh && chmod +x acme.sh" -s /bin/bash
-  su letsencrypt -c "/home/letsencrypt/acme.sh email=andrejsoucek@seznam.cz --force && rm acme.sh" -s /bin/bash
-  su letsencrypt -c "/home/letsencrypt/.acme.sh/acme.sh --issue -d pocketpilot.cz --nginx" -s /bin/bash
-  su letsencrypt -c "/home/letsencrypt/.acme.sh/acme.sh --install-cert -d pocketpilot.cz --key-file /home/letsencrypt/cert/key.pem --fullchain-file /home/letsencrypt/cert.pem --reloadcmd \"service nginx force-reload\""
+set -e
+
+DOMAIN='pocketpilot.cz'
+
+if [ -d /home/letsencrypt/.acme.sh ]; then
+	mkdir -p /home/letsencrypt/cert /home/letsencrypt/wwwroot
+	curl https://get.acme.sh -o /home/letsencrypt/acme.sh && chmod +x acme.sh
+	/home/letsencrypt/acme.sh email=andrejsoucek@seznam.cz --force
+	rm /home/letsencrypt/acme.sh
+	/home/letsencrypt/.acme.sh/acme.sh --issue -d "$DOMAIN" -w /home/letsencrypt/wwwroot
+	/home/letsencrypt/.acme.sh/acme.sh --install-cert -d "$DOMAIN" --key-file /home/letsencrypt/cert/key.pem --fullchain-file /home/letsencrypt/cert.pem --reloadcmd "sudo service nginx force-reload"
 fi
 
 while true; do
   sleep 86400
-  /home/letsencrypt/.acme.sh/acme.sh --cron --home /home/letsencrypt/.acme.sh
+  /home/letsencrypt/.acme.sh/acme.sh --cron --home /home/letsencrypt/.acme.sh || true
 done
